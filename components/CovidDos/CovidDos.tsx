d3be496abc91146725f647f0a8e2b7c92dacb8b9
Brian Boyko <brian.boyko@publicissapient.com>
2020-03-23 14:44:23 +0000

    Minor changes to formatting

--- components/CovidDos/CovidDos.tsx
+++ components/CovidDos/CovidDos.tsx
@@ -1,6 +1,17 @@
 import React from "react";
 import styled from "styled-components";
 
+const StyledRule = styled.li`
+  padding-bottom: 10px;
+`
+const StyledRuleList = styled.ul`
+  padding-top: 20px;
+  max-width: 600px;
+  width: 90%;
+  text-align: left;
+  margin: auto;
+  padding-bottom: 15px;
+`
 const CovidDos: React.FC<{}> = () => {
   return (
     <div>
@@ -9,28 +20,19 @@ const CovidDos: React.FC<{}> = () => {
         Failure to follow these rules may result in infection and worse,
         disqualification from the task.
       </div>
-      <div>
-        <ul>
-          <li>All the tasks should take place in your homes or gardens.</li>
-          <li>
-            Obey the social distancing advice and keep at least 2 meters (about
-            37 Callipos) away from people not in your immediate family.
-          </li>
-          <li>Do not break the law.</li>
-          <li>Don't hurt anyone</li>
 
-          <li>
-            Before and each task, and as necessary during the task, wash your
-            hands with soap and water.
-          </li>
-          <li>
-            You must wash your hands for at least 20 seconds, while singing,
-            humming, or whistling the Taskmaster theme tune.
-          </li>
-          <li>Use your imagination.</li>
-          <li>Have fun.</li>
-        </ul>
-      </div>
+      <StyledRuleList>
+        <StyledRule>All the tasks should take place in your homes or gardens.</StyledRule>
+        <StyledRule>
+          Obey the social distancing advice and keep at least 2 meters (about
+          3.97 baby rhinoceri) away from people not in your household.
+        </StyledRule>
+        <StyledRule>Do not break the law.</StyledRule>
+        <StyledRule>Don't hurt anyone.</StyledRule>
+        <StyledRule>Make good choices.</StyledRule>
+        <StyledRule>Use your imagination.</StyledRule>
+        <StyledRule>Have fun.</StyledRule>
+      </StyledRuleList>
     </div>
   );
 };
--- pages/tasks/[id].tsx
+++ pages/tasks/[id].tsx
@@ -1,16 +1,21 @@
 import React from "react";
 import TimeStartsNow from "../../components/TimeStartsNow";
 import { useRouter } from "next/router";
+import NoTaskYet from "./no-task-yet";
+
+const SwitchTasks: React.FC<{ id: any }> = ({ id }) => {
+  const parsedId = parseInt(id, 10);
+  switch (parsedId) {
+    case NaN:
+    default:
+      return <NoTaskYet id={id} />;
+  }
+};
 
 const Tasks: React.FC<{}> = () => {
   const router = useRouter();
   const { id } = router.query;
-  return (
-    <div>
-      <TimeStartsNow />
-      <div>{id}</div>
-    </div>
-  );
+  return <SwitchTasks id={id} />;
 };
 
 export default Tasks;
