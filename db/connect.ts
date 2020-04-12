22ab669e648ba8aa2c3d95daade69b984fd0b54e
brianboyko <brian.boyko@gmail.com>
2020-04-12 14:49:17 +0100

    URL console logging

--- db/connect.ts
+++ db/connect.ts
@@ -4,6 +4,7 @@ const URL = process.env.MONGODB_URI;
 
 const connect = (): Promise<MongoClient> =>
   new Promise((resolve, reject) => {
+    console.log("URL!", URL);
     MongoClient.connect(URL, (err, db: MongoClient) => {
       if (err) {
         reject(err);
